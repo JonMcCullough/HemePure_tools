31ffe28b8fb2104d579ba29cea41121f45aad271
Sharp C. Y. Lo <sharp.cy.lo@gmail.com>
2022-06-01 11:13:48 +0100

    Improving Efficiency
    In the original version, the sed command for inserting a header to each separated file is inefficient when many time steps are involved. The changes in this commit improve the efficiency by writing the header while separating the time series.

--- postProcessTools/toParaview.sh
+++ postProcessTools/toParaview.sh
@@ -7,13 +7,29 @@ processing () {
 
 	echo Processing $NAME for ParaView, to be called $OUT.
 
-	sed -i '1,2d' $1
+	HEADER='1isteps gridX gridY gridZ velX velY velZ pressure coverageFactor'
+
+	# Delete all empty lines
 	sed -i '/^$/d' $1
 
-	#awk -F ' ' 'BEGIN{N=-1; last_step=-1}{if(last_step != $1) {N++; print>$OUT"N".txt"; last_step=$1;} else {print>>$OUT"N".txt"}}' $NAME
-	awk -v out=$2 -F ' ' 'BEGIN{N=-2; last_step=-1}{if(last_step != $1) {N++; print>out N".txt"; last_step=$1;} else {print>>out N".txt"}}' $NAME
-
-	sed -i '1isteps gridX gridY gridZ velX velY velZ pressure coverageFactor' $OUT*
+	# Separate time series into individual files
+	awk -v var=$OUT -v header="$HEADER" -F ' ' \
+	'
+    	BEGIN{N=-2; last_step=-1}
+    	{
+	        if(last_step != $1)
+        	{
+            	N++;
+            	print header > var N".txt";
+            	print >> var N".txt";
+            	last_step=$1;
+        	}
+        	else
+        	{
+            	print >> var N".txt"
+        	}
+    	}
+	' $NAME
 }
 
 rm results_0/outlet*
